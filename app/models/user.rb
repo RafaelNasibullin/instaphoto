26722b36f09d5a4e89dfbea4277d8d47fa37b1b4
RubyBorei <human1241@gmail.com>
2017-03-14 16:49:27 +0600

    Added profile controller with show,edit actions. Added avatar and bio to our user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-  			:recoverable, :rememberable, :trackable, :validatable
+ 				 :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts, dependent: :destroy 
   has_many :comments, dependent: :destroy
+  
+  has_attached_file :avatar, styles: { medium: '152x152#' }  
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/  
 
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'profiles/show'
+
 	devise_for :users, :controllers => { registrations: 'registrations' }
 	root 'posts#index'
   resources :posts do
   	resources :comments
   end
+  get ':user_name', to: 'profiles#show', as: :profile
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
