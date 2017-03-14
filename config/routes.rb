f6682e450dba7e0112fe1f9ad3bbe805c638cdaf
RubyBorei <human1241@gmail.com>
2017-03-14 17:16:08 +0600

    Made profile workable to edit profile and look at it

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   end
   get ':user_name', to: 'profiles#show', as: :profile
   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
